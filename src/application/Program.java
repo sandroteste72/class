4e7ff7b6dc4d90a657fde6cbc480140be1af2614
sandroteste72 <sandro.teste72@gmail.com>
2021-10-18 19:18:27 -0300

    Creating and Listing Directories and Files

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,9 +2,11 @@ package application;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Program {
 
@@ -28,26 +30,52 @@ public class Program {
 		 * e.printStackTrace(); } }
 		 */
 
-		String path = "C:\\ws_eclipse\\in.txt";
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			String line = br.readLine();
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
-			}
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+//		String path = "C:\\ws_eclipse\\in.txt";
+//		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+//			String line = br.readLine();
+//			while (line != null) {
+//				System.out.println(line);
+//				line = br.readLine();
+//			}
+//		} catch (IOException e) {
+//			System.out.println("Error: " + e.getMessage());
+//		}
+//
+//		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
+//		path = "C:\\ws_eclipse\\out.txt";
+//		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+//			for (String line : lines) {
+//				bw.write(line);
+//				bw.newLine();
+//			}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter a folder path: ");
+		String strPath = scan.nextLine();
+
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		
+		System.out.println("FOLDERS:");
+		for (File folder : folders) {
+			System.out.println(folder);
+		}
+		
+		File[] files = path.listFiles(File::isFile);
+		
+		System.out.println("FILES:");
+		for (File file : files) {
+			System.out.println(file);
 		}
 
-		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
-		path = "C:\\ws_eclipse\\out.txt";
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
-			for (String line : lines) {
-				bw.write(line);
-				bw.newLine();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		boolean success = new File(strPath + "\\subdir").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		
+		scan.close();
 	}
 }
